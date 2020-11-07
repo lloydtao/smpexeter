2d8301c47c570e96ac26a6e262929158ca4d24d0
LloydTao <LL23@live.co.uk>
2020-11-07 18:52:15 +0000

    Add start-up script

---
+++ survival/start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+memory="1024"
+server="server/paper-262.jar"
+
+echo "======== SMPExeter Minecraft Server ========"
+echo "Initialising SMPExeter server start-up."
+echo "Server binary: "$server
+echo "Allocated RAM: "$memory"MB"
+echo "--------------------------------------------"
+
+echo "eula=true" > eula.txt
+
+if [ -e $server ]
+then 
+    echo "Found: "$server
+    minimumsize=128000
+    actualsize=$(wc -c <"$server")
+    if [ $actualsize -lt $minimumsize ]
+    then
+        echo $server" does not appear to be a valid binary."
+    else
+        echo "Launching Minecraft server."
+        java -Xms128M -Xmx"$memory"M -jar "$server"
+    fi
+else
+    echo "Could not find: "$server
+fi
