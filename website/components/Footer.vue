abcfcd723caef7f969fda56880d9d6c40dbdda44
LloydTao <LL23@live.co.uk>
2020-11-17 23:06:05 +0000

    Create footer component

---
+++ website/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer class="mx-auto py-16 px-3 bg-blue-700 text-gray-200">
+    <div class="container flex justify-between mx-auto">
+      <div class="flex-1 px-3">
+        <h2 class="text-lg font-semibold">About Us</h2>
+        <p class="mt-5">
+          Founded in 2019, the Exeter Minecraft Society runs multiplayer servers
+          for students at Exeter.
+        </p>
+      </div>
+      <div class="flex-1 px-3">
+        <h2 class="text-lg font-semibold">Important Links</h2>
+        <ul class="mt-4 leading-loose">
+          <li><a href="#">Terms &amp; Conditions</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+        </ul>
+      </div>
+      <div class="flex-1 px-3">
+        <h2 class="text-lg font-semibold">Social Media</h2>
+        <ul class="mt-4 leading-loose">
+          <li><a href="https://www.facebook.com/SMPExeter/">Facebook</a></li>
+          <li><a href="https://discord.gg/W6WsBZAAqZ">Discord</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
